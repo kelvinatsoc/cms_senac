28d5ad686a734716eb6f05f76c60bec745d72108
Kelvin Costa <kelvinnascimentocosta@live.com>
2020-11-03 20:18:54 -0300

    aula do dia 27, continuando o primeiro plugin

--- meu_primeiro_plugin.php
+++ meu_primeiro_plugin.php
@@ -12,4 +12,23 @@ add_filter( 'login_errors', 'altera_msg_login'){
     function altera_msg_login(){
         return 'Credenciais invalidas';
     }
-};
+}
+
+add_function colocar_og_tags(){
+    if(is_single()){
+        $dados_do_post = get_post(get_the_ID());
+        $nome_do_site = get_bloginfo();
+        
+        $titulo = $dados_do_post -> post_title;
+        $resumo = $dados_do_post -> post_excerpt;
+
+        echo "\n\n\n
+        <meta property='og:title' content'" . $titulo ."' >\n
+        <meta property='og:site_name' content'" . $nome_do_site ."' >\n
+        <meta property='og:url' content'" . $get_permalink() ."' >\n
+        <meta property='og:description' content'" . $resumo ."' >\n
+        \n\n\n";
+
+
+    }
+}
